e629e1f4dd1c7d4af1db0adef310e6c5e5be1434
sue06004 <sue06004@naver.com>
2024-09-06 10:54:09 +0900

    [Init] 프론트 프로젝트 초기 생성

---
+++ frontend/src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+import piniaPersistedstate from "pinia-plugin-persistedstate";
+
+const pinia = createPinia();
+pinia.use(piniaPersistedstate);
+const app = createApp(App);
+
+app.use(pinia);
+app.use(router);
+
+app.mount('#app')
+
