94a3ef475b03074d7d4867c43dfd82b8c92365b7
jimnyy <dojimny1004@gmail.com>
2024-09-06 16:09:29 +0900

    [Feat] 위키 등록 컴포넌트, 페이지 개발 (#78)

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
+import WikiRegisterPage from "@/pages/wiki/WikiRegisterPage.vue";
 
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: []
+  routes: [
+    { path: "/wiki", component: WikiRegisterPage }
+    , 
+  ]
 });
 
 export default router;
