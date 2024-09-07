1170025a55e088ff463110dfed873935529b95f4
sue06004 <sue06004@naver.com>
2024-09-07 13:32:59 +0900

    [Feat] 이미지 업로드 관련 BaseResponseStatus 추가 (#41)

--- backend/src/main/java/org/example/backend/Common/BaseResponseStatus.java
+++ backend/src/main/java/org/example/backend/Common/BaseResponseStatus.java
@@ -24,8 +24,12 @@ public enum BaseResponseStatus {
     // 카테고리 기능 - 9000
 
     // 기타 기능 - 10000
+    EMPTY_FILE(false, 10011, "빈 파일입니다."),
+    INVALID_FILE_TYPE(false, 10012, "지원하지 않는 파일 형식입니다."),
+    EXCEED_MAX_SIZE(false, 10013, "파일의 크기가 20MB를 넘습니다."),
 
-    // 실패 - 40000
+
+    // 실패 - 40000 (위치 바꾸지 마시오)
     FAIL(false, 40000, "요청에 실패하였습니다.");
 
     private final boolean isSuccess;
--- backend/src/main/java/org/example/backend/File/CloudFileUploadService.java
+++ backend/src/main/java/org/example/backend/File/CloudFileUploadService.java
@@ -26,6 +26,7 @@ public class CloudFileUploadService {
     private String region;
 
     private static final List<String> IMAGE_EXTENSIONS = Arrays.asList("png", "jpg", "jpeg", "gif", "webp", "bmp", "svg");
+    private static final Long MAX_IMG_FILE_SIZE = 20 * 1024 * 1024L;
 
     private String makeFolder() {
         return "IMAGE/" + LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
@@ -35,6 +36,11 @@ public class CloudFileUploadService {
         if (file == null || file.isEmpty()) { // 빈 파일인지 검사
             return false;
         }
+
+        if (file.getSize() > MAX_IMG_FILE_SIZE) { // 업로드한 파일의 크기 검사
+            return false;
+        }
+
         String fileContentType = file.getContentType();
         if (fileContentType == null || !fileContentType.startsWith("image/")) { // 파일의 MIME 타입을 검사
             return false;
