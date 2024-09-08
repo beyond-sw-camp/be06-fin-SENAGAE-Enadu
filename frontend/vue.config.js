3117803384009d3e50090ac3c3c2bc91b9e92349
sue06004 <sue06004@naver.com>
2024-09-08 18:02:36 +0900

    [Feat] 채팅방 조회 api 연결 및 시간 동적 표시 (#85)

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        pathRewrite: { "^/api": ""}
+      }
+    }
+  },
   transpileDependencies: true
 })
