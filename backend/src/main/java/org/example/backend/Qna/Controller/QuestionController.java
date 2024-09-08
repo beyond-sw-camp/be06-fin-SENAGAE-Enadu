cc2a987e22aaa7c83157b78170c58387287895e8
ashd89 <jyjhlee0809@gmail.com>
2024-09-09 01:48:02 +0900

    [Feat] QnA 목록 조회 api 구현

--- backend/src/main/java/org/example/backend/Qna/Controller/QuestionController.java
+++ backend/src/main/java/org/example/backend/Qna/Controller/QuestionController.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.example.backend.Category.Model.Res.SubCategoryRes;
 import org.example.backend.Common.BaseResponse;
 import org.example.backend.Qna.Service.QnaService;
+import org.example.backend.Qna.model.Entity.Res.GetQnaListRes;
 import org.example.backend.Qna.model.Entity.req.CreateQuestionReq;
+import org.example.backend.Qna.model.Entity.req.GetQnaListReq;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,5 +22,13 @@ public class QuestionController {
     public BaseResponse<Void> saveQuestion(@RequestBody CreateQuestionReq req) {
         qnaService.saveQuestion(req);
         return new BaseResponse<>();
+    }
+
+    //qna 목록 조회
+    @GetMapping("list")
+    public BaseResponse<List<GetQnaListRes>> getQnaList(@RequestBody GetQnaListReq req) {
+        List<GetQnaListRes> qnaListRes = qnaService.getQnaList(req);
+        return new BaseResponse<>(qnaListRes);
+
     }
 }
