7d5544e412395e767bf3423b296f2fa7dbac1056
sue06004 <sue06004@naver.com>
2024-09-08 16:09:54 +0900

    [Refactor] 불필요한 import 삭제 (#55)

--- backend/src/main/java/org/example/backend/Chat/Service/ChatService.java
+++ backend/src/main/java/org/example/backend/Chat/Service/ChatService.java
@@ -8,7 +8,6 @@ import org.example.backend.Chat.Repository.ChatRoomRepository;
 import org.example.backend.User.Model.Entity.User;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
