b3f06b3654988f61eab21220c5dedca03f721e5d
ashd89 <jyjhlee0809@gmail.com>
2024-09-09 17:41:25 +0900

    [Feat] 질문 등록 페이지 구현 및 백엔드 api 연결

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,13 +2,16 @@ import { createRouter, createWebHistory } from "vue-router";
 import QnaListPage from "@/pages/QnaListPage.vue";
 import ChatNavComponent from "@/components/Chat/ChatNavComponent.vue";
 import WikiRegisterPage from "@/pages/wiki/WikiRegisterPage.vue";
+import QnaRegisterComponent from "@/components/qna/QnaRegisterComponent.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/qna-list", component: QnaListPage },
+    { path: "/qna/register", component: QnaRegisterComponent },
     { path: "/wiki", component: WikiRegisterPage },
     { path: "/chat", component: ChatNavComponent },
+
   ]
 });
 
