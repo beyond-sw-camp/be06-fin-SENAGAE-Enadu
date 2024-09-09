c7696bd3ed085d3fd002542effa1c92eca0bac64
ashd89 <jyjhlee0809@gmail.com>
2024-09-09 15:53:13 +0900

    [Fix] Qna 질문 등록 기능 CustomUserDetails를 활용한 유저 데이터 연결

--- backend/src/main/java/org/example/backend/Qna/Service/QnaService.java
+++ backend/src/main/java/org/example/backend/Qna/Service/QnaService.java
@@ -4,13 +4,16 @@ import lombok.RequiredArgsConstructor;
 import org.example.backend.Category.Model.Entity.Category;
 import org.example.backend.Category.Repository.CategoryRepository;
 import org.example.backend.Common.BaseResponseStatus;
-import org.example.backend.Exception.custom.InvalidFileException;
 import org.example.backend.Exception.custom.InvalidQnaException;
+import org.example.backend.Exception.custom.InvalidUserException;
 import org.example.backend.Qna.Repository.QuestionRepository;
 import org.example.backend.Qna.model.Entity.QnaBoard;
 import org.example.backend.Qna.model.Entity.Res.GetQnaListRes;
 import org.example.backend.Qna.model.Entity.req.CreateQuestionReq;
 import org.example.backend.Qna.model.Entity.req.GetQnaListReq;
+import org.example.backend.Security.CustomUserDetails;
+import org.example.backend.User.Model.Entity.User;
+import org.example.backend.User.Repository.UserRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -26,29 +29,34 @@ import java.util.stream.Collectors;
 public class QnaService {
     private final QuestionRepository questionRepository;
     private final CategoryRepository categoryRepository;
+    private final UserRepository userRepository;
 
-    public void saveQuestion(CreateQuestionReq req) {
-        Optional<Category> category = categoryRepository.findById(req.getCategoryId());
+    public void saveQuestion(CreateQuestionReq createQuestionReq, CustomUserDetails customUserDetails) {
+        Optional<Category> category = categoryRepository.findById(createQuestionReq.getCategoryId());
+        Optional<User> user = userRepository.findByEmail(customUserDetails.getUsername());
 
-        if (category.isPresent()) {
+        if (category.isPresent() && user.isPresent()) {
             QnaBoard qnaBoard = QnaBoard.builder()
-                    .title(req.getTitle())
-                    .content(req.getContent())
+                    .user(user.get())
+                    .title(createQuestionReq.getTitle())
+                    .content(createQuestionReq.getContent())
                     .category(category.get())
                     .build();
 
             qnaBoard.createdAt();
             questionRepository.save(qnaBoard);
         }
-        else {
+        else if (category.isEmpty()) {
             throw new InvalidQnaException(BaseResponseStatus.INVALID_CATEGORY_DATA);
+        } else {
+            throw new InvalidUserException(BaseResponseStatus.USER_NOT_FOUND);
         }
     }
 
     public List<GetQnaListRes> getQnaList(GetQnaListReq req) {
         Pageable pageable;
         // 최신순 or 좋아요가 많은 순으로 정렬 type 지정, 둘 다 아니면 에러
-        if(req.getSort().equals("latest")) {
+        if (req.getSort().equals("latest")) {
             pageable = PageRequest.of(req.getPage(), req.getSize(), Sort.by(Sort.Direction.DESC, "createdAt"));
         }
         else if(req.getSort().equals("like")) {
