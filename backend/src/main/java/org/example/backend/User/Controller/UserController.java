8717862cd5334775250a61c0999f7aa20d9cc5ad
Dayun Kim <97131066+dyun23@users.noreply.github.com>
2024-09-11 01:09:21 +0900

    [Feat] 로그아웃 추가 (#160)

--- backend/src/main/java/org/example/backend/User/Controller/UserController.java
+++ backend/src/main/java/org/example/backend/User/Controller/UserController.java
@@ -1,4 +1,23 @@
 package org.example.backend.User.Controller;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.example.backend.Common.BaseResponse;
+import org.example.backend.Util.JwtUtil;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
+    private final JwtUtil jwtUtil;
+    @PostMapping("/logout")
+    public BaseResponse<String> logout(HttpServletResponse response) {
+        Cookie expiredCookie = jwtUtil.removeCookie();
+        response.addCookie(expiredCookie);
+        return new BaseResponse<>("로그아웃");
+    }
 }
