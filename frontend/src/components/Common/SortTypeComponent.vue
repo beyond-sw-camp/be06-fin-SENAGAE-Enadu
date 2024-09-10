5575854401c0dae463978b8dc01529f46de759de
ashd89 <jyjhlee0809@gmail.com>
2024-09-10 16:52:30 +0900

    [Feat] 정렬 유형 선택 컴포넌트 추가

---
+++ frontend/src/components/Common/SortTypeComponent.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="radio-input">
+    <label>
+      <input value="value-1" name="value-radio" id="value-1" type="radio" />
+      <span>최신 순</span>
+    </label>
+    <label>
+      <input value="value-2" name="value-radio" id="value-2" type="radio" />
+      <span>좋아요 순</span>
+    </label>
+    <span class="selection"></span>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+.radio-input input {
+  display: none;
+}
+
+.radio-input {
+  --container_width: 300px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  border-radius: 10px;
+  background-color: #fff;
+  color: #000000;
+  width: var(--container_width);
+  overflow: hidden;
+  border: 1px solid rgba(53, 52, 52, 0.226);
+}
+
+.radio-input label {
+  width: 100%;
+  padding: 10px;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+  font-weight: 600;
+  letter-spacing: -1px;
+  font-size: 14px;
+}
+
+.selection {
+  display: none;
+  position: absolute;
+  height: 100%;
+  width: calc(var(--container_width) / 2);
+  z-index: 0;
+  left: 0;
+  top: 0;
+  transition: 0.15s ease;
+}
+
+.radio-input label:has(input:checked) {
+  color: #fff;
+}
+
+.radio-input label:has(input:checked) ~ .selection {
+  background-color: rgb(11 117 223);
+  display: inline-block;
+}
+
+.radio-input label:nth-child(1):has(input:checked) ~ .selection {
+  transform: translateX(calc(var(--container_width) * 0 / 2));
+}
+
+.radio-input label:nth-child(2):has(input:checked) ~ .selection {
+  transform: translateX(calc(var(--container_width) * 1 / 2));
+}
+
+</style>
