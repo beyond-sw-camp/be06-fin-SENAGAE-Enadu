e4a6f12042d4390911cb68531c387dbb87a7b8db
Dayun Kim <97131066+dyun23@users.noreply.github.com>
2024-09-12 15:37:25 +0900

    [Fix] custom-container 수정

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -28,4 +28,13 @@
     background-color: #fff;
     --main-color: #1e88e5;
   }
+  .custom-container {
+    width: 75%;
+    max-width: 1400px;
+    margin: 50px auto;
+    /* background-color: #fff; */
+    padding: 30px;
+    border-radius: 10px;
+    /* box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); */
+  }
 </style>
