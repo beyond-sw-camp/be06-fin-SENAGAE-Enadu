43d20dc462c5ec8fcdec068830b208a299c60bbc
Dayun Kim <97131066+dyun23@users.noreply.github.com>
2024-09-12 11:01:49 +0900

    [Feat] 이메일, 닉네임 중복 확인 (#14,#15)

--- backend/src/main/java/org/example/backend/User/Service/UserService.java
+++ backend/src/main/java/org/example/backend/User/Service/UserService.java
@@ -1,4 +1,18 @@
 package org.example.backend.User.Service;
 
+import lombok.RequiredArgsConstructor;
+import org.example.backend.User.Repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+    public Boolean searchEmail(String email) {
+        return userRepository.findByEmail(email).isEmpty();
+    }
+
+    public Boolean searchNickname(String nickname) {
+        return userRepository.findByNickname(nickname).isEmpty();
+    }
 }
