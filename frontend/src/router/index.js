fde7461a6a2375ce7f94e9431784bd2734a84677
jimnyy <dojimny1004@gmail.com>
2024-09-14 18:26:37 +0900

    [Feat] 위키 상세조회 기능 (#86)

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,6 +5,7 @@ import WikiRegisterPage from "@/pages/WikiRegisterPage.vue";
 import ChatPage from "@/pages/ChatPage.vue";
 import QnaRegisterComponent from "@/components/qna/QnaRegisterComponent.vue";
 import OAuthLoginPage from "@/pages/OAuthLoginPage.vue";
+import WikiDetailPage from "@/pages/WikiDetailPage.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -14,7 +15,8 @@ const router = createRouter({
     { path: "/qna/register", component: QnaRegisterComponent },
     { path: "/wiki", component: WikiRegisterPage },
     { path: "/chat", component: ChatPage },
-    { path: "/oauth", component: OAuthLoginPage, meta: { showHeader: false } }
+    { path: "/oauth", component: OAuthLoginPage, meta: { showHeader: false } },
+    { path: "/wiki/detail", component: WikiDetailPage }
   ]
 });
 
