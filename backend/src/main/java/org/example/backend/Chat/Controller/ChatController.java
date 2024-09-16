de57cef7b722324b5bd7dff70c2a0bdbc93a09b7
sue06004 <sue06004@naver.com>
2024-09-16 17:32:59 +0900

    [Feat] 채팅 시작 API 구현 (#52)

--- backend/src/main/java/org/example/backend/Chat/Controller/ChatController.java
+++ backend/src/main/java/org/example/backend/Chat/Controller/ChatController.java
@@ -1,15 +1,15 @@
 package org.example.backend.Chat.Controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.backend.Chat.Model.Req.StartChatReq;
 import org.example.backend.Chat.Model.Res.GetChatMessageRes;
 import org.example.backend.Chat.Model.Res.GetChatRoomRes;
+import org.example.backend.Chat.Model.Res.StartChatRes;
 import org.example.backend.Chat.Service.ChatService;
 import org.example.backend.Common.BaseResponse;
 import org.example.backend.Security.CustomUserDetails;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,7 +28,11 @@ public class ChatController {
     public BaseResponse<List<GetChatMessageRes>> getChatMessageList(@AuthenticationPrincipal CustomUserDetails customUserDetails,
                                                                     Long chatRoomId, Integer page, Integer size) {
         return new BaseResponse<>(chatService.getChatMessageList(customUserDetails.getUserId(), chatRoomId, page, size));
-
     }
 
+    @PostMapping("/start")
+    public BaseResponse<StartChatRes> createChatRoom(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody StartChatReq startChatReq){
+        System.out.println(startChatReq.getNickname());
+        return new BaseResponse<>(chatService.startChat(customUserDetails.getUserId(), startChatReq));
+    }
 }
--- backend/src/main/java/org/example/backend/Chat/Repository/ChatRoomRepository.java
+++ backend/src/main/java/org/example/backend/Chat/Repository/ChatRoomRepository.java
@@ -4,8 +4,10 @@ import org.example.backend.Chat.Model.Entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
     List<ChatRoom> findAllByUser1Id(Long userId);
     List<ChatRoom> findAllByUser2Id(Long userId);
+    Optional<ChatRoom> findByUser1IdAndUser2Id(Long userId, Long user2Id);
 }
