d26acf8936f30acfb6ede12683de2b3b3c25aaa6
Dayun Kim <97131066+dyun23@users.noreply.github.com>
2024-09-23 17:39:16 +0900

    [Fix] 어노테이션 수정

--- backend/src/main/java/org/example/backend/User/Model/Req/UserSignupReq.java
+++ backend/src/main/java/org/example/backend/User/Model/Req/UserSignupReq.java
@@ -2,19 +2,12 @@ package org.example.backend.User.Model.Req;
 
 
 import jakarta.validation.constraints.Email;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import org.hibernate.annotations.processing.Pattern;
 
 @Getter
-@Builder
-@AllArgsConstructor
 public class UserSignupReq {
     private String nickname;
     @Email
     private String email;
     private String password;
-
-
 }
