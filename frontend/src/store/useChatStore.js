5c35a8dcfddd154f8f5e4addacdf7ecec9a75238
Dayun Kim <97131066+dyun23@users.noreply.github.com>
2024-09-24 11:07:30 +0900

    [Fix] 백엔드 요청 주소 수정

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -38,7 +38,7 @@ export const useChatStore = defineStore("chat", {
             const startChatReq = {
                 nickname: nickname,
             }
-            const res = await axios.post(`${backend}/chat/start`, startChatReq, {withCredentials: true});
+            const res = await axios.post(backend + "/chat/start", startChatReq, {withCredentials: true});
             this.selectedChatRoom = res.data.result;
         },
         async getChatRoomList() {
@@ -78,7 +78,7 @@ export const useChatStore = defineStore("chat", {
 
         },
         connect() {
-            const serverUrl = "http://localhost:8080/ws/chat";
+            const serverUrl = backend +"/ws/chat";
             let socket = new SockJS(serverUrl, null, {withCredentials: true});
             this.stompClient = Stomp.over(socket);
             console.log(`소켓 연결을 시도합니다. 서버 주소: ${serverUrl}`)
@@ -148,7 +148,7 @@ export const useChatStore = defineStore("chat", {
         },
         updateChatRoomData(chatRoomId, message, sendTime) {
             for (const chatRoom of this.chatRoomList) {
-                if (chatRoom.chatRoomId == chatRoomId) {
+                if (chatRoom.chatRoomId === chatRoomId) {
                     chatRoom.lastMessage = message;
                     chatRoom.lastMessageDay = sendTime
                 }
