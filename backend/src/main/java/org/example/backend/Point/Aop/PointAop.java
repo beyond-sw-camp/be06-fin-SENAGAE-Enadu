cd5f1d0804cd65b163e1537a2c8425956a9997cf
sue06004 <sue06004@naver.com>
2024-10-06 15:51:36 +0900

    [Feat] 답변 채택 포인트 부여 AOP 등록 (#423)

--- backend/src/main/java/org/example/backend/Point/Aop/PointAop.java
+++ backend/src/main/java/org/example/backend/Point/Aop/PointAop.java
@@ -4,8 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
+import org.example.backend.Common.BaseResponseStatus;
+import org.example.backend.Exception.custom.InvalidQnaException;
 import org.example.backend.Point.Model.Enum.PointDescriptionEnum;
 import org.example.backend.Point.Service.PointService;
+import org.example.backend.Qna.Repository.AnswerRepository;
+import org.example.backend.Qna.model.Entity.Answer;
 import org.example.backend.Security.CustomUserDetails;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +18,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class PointAop {
     private final PointService pointService;
+    private final AnswerRepository answerRepository;
 
     @AfterReturning("execution(* org.example.backend.Qna.Service.QnaService.saveQuestion(..))")
     public void givePointAfterSuccessQnaRegister(JoinPoint joinPoint) {
@@ -37,6 +42,15 @@ public class PointAop {
     public void givePointAfterSuccessAnswerRegister(JoinPoint joinPoint) {
         Object[] args = joinPoint.getArgs();
         pointService.givePoint((Long) args[1], PointDescriptionEnum.POINT_QNA_ANSWER_WRITE);
+    }
+
+    @AfterReturning("execution(* org.example.backend.Qna.Service.QnaService.adoptedAnswer(..))")
+    public void givePointAfterSuccessAnswerAdopt(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        Long answerId = (Long) args[1];
+        Answer answer = answerRepository.findById(answerId).get(); //AfterReturning이므로 무조건 있음
+        Long userId = answer.getUser().getId();
+        pointService.givePoint(userId, PointDescriptionEnum.POINT_QNA_ANSWER_ACCEPT);
     }
 
     //todo - 질문 글 답변 작성, 답변 채택, 추천 10 이상, 비추천 5이상
