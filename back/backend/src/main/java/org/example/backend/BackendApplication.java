e2d4f19f2a8e0a151069416533f8c397fb727847
sue06004 <sue06004@naver.com>
2024-10-10 21:00:14 +0900

    [Feat] backend-deployment 시크릿 추가 및 백엔드 시간 설정

--- back/backend/src/main/java/org/example/backend/BackendApplication.java
+++ back/backend/src/main/java/org/example/backend/BackendApplication.java
@@ -1,11 +1,14 @@
 package org.example.backend;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -20,5 +23,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init()
+	{
+		// JVM의 기본 시간대를 Asia/Seoul로 설정
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 }
