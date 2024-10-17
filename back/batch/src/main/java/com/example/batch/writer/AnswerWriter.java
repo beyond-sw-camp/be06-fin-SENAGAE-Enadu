ecbf13fbbe7cdc1628512ff8ca55d399f76dbd58
Dayun Kim <97131066+dyun23@users.noreply.github.com>
2024-10-18 00:19:43 +0900

    [Refactor] log로 변경

--- back/batch/src/main/java/com/example/batch/writer/AnswerWriter.java
+++ back/batch/src/main/java/com/example/batch/writer/AnswerWriter.java
@@ -5,11 +5,12 @@ import com.example.common.Qna.Repository.QuestionRepository;
 import com.example.common.Qna.model.Entity.Answer;
 import com.example.common.Qna.model.Entity.QnaBoard;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.stereotype.Component;
 
-
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class AnswerWriter implements ItemWriter<Answer> {
@@ -19,10 +20,12 @@ public class AnswerWriter implements ItemWriter<Answer> {
 
     @Override
     public void write(Chunk<? extends Answer> chunk) throws Exception {
-        System.out.println("Writer 실행 - 처리할 항목 수: " + chunk.size());
+        log.info("Writer 실행 - 처리할 항목 수: {}", chunk.size());
+
         for (Answer answer : chunk) {
-            System.out.println("Writer 처리 중인 항목: " + answer.getQnaBoard().getId());
+            log.info("Writer 처리 중인 항목: QnaBoard ID = {}", answer.getQnaBoard().getId());
             answerRepository.save(answer);
+
             QnaBoard qnaBoard = answer.getQnaBoard();
             if (qnaBoard != null) {
                 qnaBoard.increaseAnswerCount();
