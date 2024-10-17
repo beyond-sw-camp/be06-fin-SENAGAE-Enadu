2c1840d6aa7c436c72f33921ba62cb22c3c8c6c5
Dayun Kim <97131066+dyun23@users.noreply.github.com>
2024-10-17 17:31:19 +0900

    [Design] 닉네임이 긴 경우 처리

--- frontend/src/components/errorarchive/ErrorArchiveCardComponent.vue
+++ frontend/src/components/errorarchive/ErrorArchiveCardComponent.vue
@@ -1,52 +1,53 @@
 <template>
   <router-link :to="{ path: '/errorarchive/detail', query: { id: errorarchiveCard.id }}">
-  <div class="PostCard_block__FTMsy">
-    <div class="header">
-      <div class="user-info">
-        <a class="PostCard_userInfo__Cu1X5" :href="'/@' + errorarchiveCard.nickname + '/posts'">
-          <img
-            loading="lazy"
-            decoding="async"
-            :src="errorarchiveCard.profileImg"
-            alt="Profile Image"
-            class="PostCard_avatar"
-          />
-        </a>
-        <div class="details">
-          <a class="nickname">{{ errorarchiveCard.nickname }}</a>
-          <div class="PostCard_subInfo__KqVkC">
-            <span>{{ formatDateTime(errorarchiveCard.createdAt) }}</span>
+    <div class="PostCard_block__FTMsy">
+      <div class="header">
+        <div class="user-info">
+          <a class="PostCard_userInfo__Cu1X5" :href="'/@' + errorarchiveCard.nickname + '/posts'">
+            <img
+                loading="lazy"
+                decoding="async"
+                :src="errorarchiveCard.profileImg"
+                alt="Profile Image"
+                class="PostCard_avatar"
+            />
+          </a>
+          <div class="details">
+            <a class="nickname">{{ errorarchiveCard.nickname }}</a>
+            <div class="PostCard_subInfo__KqVkC">
+              <span>{{ formatDateTime(errorarchiveCard.createdAt) }}</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
 
-    <div class="PostCard_content__W3lPm">
-      <div class="PostCard_title">{{ errorarchiveCard.title }}</div>
-    </div>
-
-    <div class="PostCard_categories">
-      <span>{{ errorarchiveCard.superCategory }}</span>
-      <span v-if="errorarchiveCard.subCategory">/ {{ errorarchiveCard.subCategory }}</span>
-    </div>
-
-    <div class="PostCard_footer__9J5Wd">
-      <div class="PostCard_likes___lp_I">
-        <svg viewBox="0 0 24 24">
-          <path fill="currentColor" d="m18 1-6 4-6-4-6 5v7l12 10 12-10V6z"></path>
-        </svg>
-        <span>{{ errorarchiveCard.likeCnt }}</span>
+      <div class="PostCard_content__W3lPm">
+        <div class="PostCard_title">{{ errorarchiveCard.title }}</div>
       </div>
-      <div class="PostCard_author__nickname">
-        <span>{{ errorarchiveCard.nickname }}</span>
+
+      <div class="PostCard_categories">
+        <span>{{ errorarchiveCard.superCategory }}</span>
+        <span v-if="errorarchiveCard.subCategory">/ {{ errorarchiveCard.subCategory }}</span>
+      </div>
+
+      <div class="PostCard_footer__9J5Wd">
+        <div class="PostCard_likes___lp_I">
+          <svg viewBox="0 0 24 24">
+            <path fill="currentColor" d="m18 1-6 4-6-4-6 5v7l12 10 12-10V6z"></path>
+          </svg>
+          <span>{{ errorarchiveCard.likeCnt }}</span>
+        </div>
+        <div class="PostCard_author__nickname">
+          <span>{{ errorarchiveCard.nickname }}</span>
+        </div>
       </div>
     </div>
-  </div>
   </router-link>
 </template>
 
 <script>
 import { formatDateTime } from '@/utils/FormatDate';
+
 export default {
   name: "ErrorArchiveCardComponent",
   data() {
@@ -55,9 +56,9 @@ export default {
   props: ["errorarchiveCard"],
   methods: {
     formatDateTime
-    },
-    components: {},
-    mounted() { 
+  },
+  components: {},
+  mounted() {
   },
 };
 </script>
@@ -95,6 +96,7 @@ export default {
   margin-right: 0.5rem;
   display: flex;
   align-items: center;
+  width: 35px;
 }
 
 .PostCard_avatar {
@@ -107,11 +109,15 @@ export default {
 .details {
   display: flex;
   flex-direction: column;
+  max-width: 130px;
 }
 
 .nickname {
   font-weight: bold;
   color: #212529;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 }
 
 .PostCard_subInfo__KqVkC {
@@ -139,17 +145,17 @@ export default {
 }
 
 .PostCard_categories {
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
-    color: var(--main-color);
-    font-size: 0.8em;
-    font-weight: 500;
-    text-align: right;
-    margin-left: auto;
-    white-space: nowrap;
-    max-width: 150px;
-    padding: 8px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  color: var(--main-color);
+  font-size: 0.8em;
+  font-weight: 500;
+  text-align: right;
+  margin-left: auto;
+  white-space: nowrap;
+  max-width: 150px;
+  padding: 8px;
 }
 
 .PostCard_footer__9J5Wd {
@@ -173,14 +179,23 @@ export default {
   height: .75rem;
   margin-right: .5rem;
 }
+
 .PostCard_author__nickname {
   display: flex;
   align-items: center;
   font-size: 0.875rem;
   color: #212529;
 }
+
+.PostCard_author__nickname > span {
+  max-width: 130px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
 .PostCard_categories > span {
-    margin-right: 5px;
+  margin-right: 5px;
 }
 
 </style>
